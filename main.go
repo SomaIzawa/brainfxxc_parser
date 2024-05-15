1a6a2a4137a5cb906fabb5bed415b6a8cba7e0e1
PL2304A1111 <pl2304a1111@PL2304A1111--m.local>
2024-05-15 10:11:41 +0900

    ファイル名を引数にとれるように修正

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"brainfxxk/util"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main()  {
+	args := os.Args
 	// ファイルパス取得
 	var filepath string
-	fmt.Scanf("%s", &filepath)
+	if len(args) > 1 {
+		filepath = args[1]
+	} else {
+		fmt.Scanf("%s", &filepath)
+	}
 	// テキストファイルの読み込み
 	code, err := util.ReadFile(filepath)
 	if err != nil {
@@ -20,4 +26,5 @@ func main()  {
 	brainfxxk := brainfxxk.NewParser(code ,128)
 	// 実行
 	brainfxxk.Exec()
+	//brainfxxk.ShowMemory(25)
 }
